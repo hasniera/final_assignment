69783507922c21469947cd054afb8ff935b103f2
hasniera <107098926+hasniera@users.noreply.github.com>
2022-06-09 11:07:10 +0800

    Update projectdemo1.py

--- projectdemo1.py
+++ projectdemo1.py
@@ -27,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = pd.read_csv('https://raw.githubusercontent.com/hasniera/final_assignment/main/IRIS.csv')
+iris =pd.read_csv('https://raw.githubusercontent.com/hasniera/final_assignment/main/IRIS.csv')
 X = iris.data
 Y = iris.target
 
