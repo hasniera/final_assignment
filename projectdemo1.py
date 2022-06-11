6f6d41a2006d1da98cae634c73aa9b1fea2416eb
hasniera <107098926+hasniera@users.noreply.github.com>
2022-06-11 21:45:07 +0800

    Update projectdemo1.py

--- projectdemo1.py
+++ projectdemo1.py
@@ -50,5 +50,5 @@ st.write(prediction)
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
 
-st.sidebar.image():
+#st.sidebar.image:
 st.sidebar.image("/home/rzwitch/Downloads/randy-streamlit.png", use_column_width=True)
